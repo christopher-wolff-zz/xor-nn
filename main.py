41642f70952b851bfca09b6fac26747e1335b796
Christopher Wolff <cfw13@duke.edu>
2017-11-23 13:36:59 -0500

    Implement backpropagation

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
-import numpy
-import math
-import random
+import numpy as np
+
+# training data
+training_input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+training_output = np.array([[0], [1], [1], [0]])
 
 # hyper parameters
 input_size = 2
-hidden_size = 5
+hidden_size = 3
 output_size = 1
 
-learning_rate = 0.2
-max_iterations = 15000
-
-# training data
-training_input = ((0, 0), (0, 1), (1, 0), (1, 1))
-training_output = (0, 1, 1, 0)
+num_epochs = 60000
 
 
-# activation functions - forward and backward
+# activation function - forward pass
 def sigmoid(x):
-    return 1.0/(1.0 + np.exp(-x))
+    return 1.0 / (1.0 + np.exp(-x))
 
 
+# derivative of activation function - backward pass
 def sigmoid_prime(x):
-    return sigmoid_forward(x) * (1.0 - sigmoid(x))
+    return x * (1.0 - x)
 
 
-def tanh(x):
-    return np.tanh(x)
+def main():
+    # randomly initialize weights
+    w0 = np.random.uniform(size=(input_size, hidden_size))   # hidden weights
+    w1 = np.random.uniform(size=(hidden_size, output_size))  # output weights
 
+    # training
+    for i in range(num_epochs):
+        # forward pass
+        hidden = sigmoid(np.dot(training_input, w0))  # hidden activations
+        output = sigmoid(np.dot(hidden, w1))          # output activations
+        # error
+        error = training_output - output
+        # update
+        delta_output = error * sigmoid_prime(output)
+        delta_hidden = delta_output.dot(w1.T) * sigmoid_prime(hidden)
+        w1 += hidden.T.dot(delta_output)
+        w0 += training_input.T.dot(delta_hidden)
+        # print status
+        if i % 1000 == 0:
+            print(error)
 
-def tanh_prime(x):
-    return 1.0 - x ** 2
-
-
-class xornn:
-    ''' Neural network for the XOR operation
-
-    The input layer has two neurons representing the boolean inputs.
-    The output layer has one neuron representing the output.
-    '''
-
-    input_size = 2
-    output_size = 1
-
-    def __init__(self, hidden_size=5, learning_rate=.1):
-        self.hidden_size = hidden_size
-        self.learning_rate = learning_rate
-
-        random.seed(1)
+    print("Training complete.")
+    print(f"Final output: {output}")
+    print(f"Final error: {error}")
 
 
 if __name__ == "__main__":
-    pass
+    main()
